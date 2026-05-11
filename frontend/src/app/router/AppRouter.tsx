2395551ad97172192bbce6e80b8f3d86fa3aaff7
«DarinaZotova» <zotovad21@gmail.com>
2026-05-11 16:13:48 +0300

    account

--- frontend/src/app/router/AppRouter.tsx
+++ frontend/src/app/router/AppRouter.tsx
@@ -7,34 +7,29 @@ import { RegisterPage } from '../../pages/register-page/RegisterPage';
 import { VerifyEmailPage } from '../../pages/verify-email-page/VerifyEmailPage';
 import { HomePage } from '../../pages/home-page/HomePage';
 import { TemplatesPage } from '../../pages/templates-page/TemplatesPage';
-import { ProjectsPage } from '../../pages/projects-page/ProjectsPage';
-import { EditorPage } from '../../pages/editor-page/EditorPage';
 import { WorkspaceLayout } from '../../layouts/workspace-layout/WorkspaceLayout';
-import { PublicRouteRedirect } from './PublicRouteRedirect';
+import { ProjectsPage } from '../../pages/projects-page/ProjectsPage';
+import { AccountPage } from '../../pages/account-page/AccountPage';
 
 export function AppRouter() {
     return (
         <Routes>
-            <Route element={<PublicRouteRedirect />}>
-                <Route path={ROUTES.LANDING} element={<LandingPage />} />
-                <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-                <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-                <Route
-                    path={ROUTES.VERIFY_EMAIL}
-                    element={<VerifyEmailPage />}
-                />
-            </Route>
+            <Route path={ROUTES.LANDING} element={<LandingPage />} />
+            <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+            <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+            <Route path={ROUTES.VERIFY_EMAIL} element={<VerifyEmailPage />} />
 
             <Route element={<WorkspaceLayout />}>
                 <Route path={ROUTES.HOME} element={<HomePage />} />
                 <Route path={ROUTES.PROJECTS} element={<ProjectsPage />} />
                 <Route path={ROUTES.TEMPLATES} element={<TemplatesPage />} />
-                <Route path={ROUTES.ACCOUNT} element={<HomePage />} />
+                <Route path={ROUTES.ACCOUNT} element={<AccountPage />} />
             </Route>
 
-            <Route path={ROUTES.EDITOR} element={<EditorPage />} />
-
-            <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
+            <Route
+                path="*"
+                element={<Navigate to={ROUTES.HOME} replace />}
+            />
         </Routes>
     );
 }
