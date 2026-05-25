877abc299dd9d414db6af5f0c934c723360cec3d
nobarg <koliasleshev@gmail.com>
2026-05-25 15:47:58 +0300

    Increase request body limit for project saving

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '25mb' }));
+  app.use(urlencoded({ extended: true, limit: '25mb' }));
 
   app.enableCors({
     origin: 'http://localhost:5173',
